eb526d9a38143d3a01126b9d1023afa9d05e4dd8
Egypt1123 <cairo.fenton1123@gmail.com>
2024-05-05 15:08:36 +1200

    Created Classes for the project

---
+++ pongGame/src/pongGame/Pong.java
@@ -0,0 +1,10 @@
+package pongGame;
+
+public class Pong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ pongGame/src/pongGame/Sprite.java
@@ -0,0 +1,5 @@
+package pongGame;
+
+public class Sprite {
+
+}
