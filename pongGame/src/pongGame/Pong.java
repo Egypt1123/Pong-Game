6150f99770684abedfaec2b86350398ab76f33de
Egypt1123 <cairo.fenton1123@gmail.com>
2024-05-05 16:33:31 +1200

    Added in JFrame, added in frame and set static final variables

--- pongGame/src/pongGame/Pong.java
+++ pongGame/src/pongGame/Pong.java
@@ -1,10 +1,24 @@
 package pongGame;
 
-public class Pong {
+import javax.swing.JFrame;
 
+
+public class Pong extends JFrame {
+	final static String SCREEN_TITLE = "Pong";
+	final static int SCREEN_WIDTH = 800;
+	final static int SCREEN_HEIGHT = 600;
+	public Pong() {
+		setTitle(SCREEN_TITLE);
+		setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	
 	public static void main(String[] args) {
-
-
+		new Pong();
+		
 	}
 
 }
