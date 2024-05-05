d64fe04f18b76c40a26771dfe89144fc559d524d
Egypt1123 <cairo.fenton1123@gmail.com>
2024-05-05 18:57:23 +1200

    Added getter setter methods and private vars to sprite class

--- pongGame/src/pongGame/PongPanel.java
+++ pongGame/src/pongGame/PongPanel.java
@@ -12,6 +12,10 @@ import java.awt.event.KeyListener;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
+import java.awt.Stroke;
+import java.awt.BasicStroke;
+import java.awt.BasicStroke;
+
 public class PongPanel extends JPanel implements ActionListener, KeyListener{
 	
 	private final static Color BACKGROUND_COLOR = Color.BLACK;
@@ -30,11 +34,19 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener{
 		
 	}
 	
+	private void paintDottedLine(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g.create();
+		Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] {9}, 0);
+		g2d.setStroke(dashed);
+		g2d.setPaint(Color.WHITE);
+		g2d.drawLine(getWidth() /2, 0, getWidth() /2 , getHeight());
+		g2d.dispose();
+	}	
+	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.WHITE);
-		g.fillRect(20, 20, 100, 100);
+		paintDottedLine(g);
 	}
 	
 	@Override
@@ -57,7 +69,8 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener{
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		// TODO Auto-generated method stub
+		update();
+		repaint();
 		
 	}
 	
--- pongGame/src/pongGame/Sprite.java
+++ pongGame/src/pongGame/Sprite.java
@@ -1,5 +1,67 @@
 package pongGame;
 
-public class Sprite {
+import java.awt.Color;
 
+public class Sprite {
+	private int xPosition, yPosition;
+	private int xVelocity, yVelocity;
+	private int width, height;
+	private Color color;
+	
+	
+	public void setXPosition(int num) {
+		this.xPosition = num;
+	}
+	
+	public void setYPosition(int num) {
+		this.yPosition = num;
+	}
+	
+	public void setXVelocity(int num) {
+		this.xVelocity = num;
+	}
+	
+	public void setYVelocity(int num) {
+		this.yVelocity = num;
+	}
+	
+	public void setWidth(int num) {
+		this.width = num;
+	}
+	
+	public void setHight(int num) {
+		this.height = num;
+	}
+	
+	public int getXPosition() {
+		return this.xPosition;
+	}
+	
+	public int getYPosition() {
+		return this.yPosition;
+	}
+	
+	public int getXVelocity() {
+		return this.xVelocity;
+	}
+	
+	public int getYVelocity() {
+		return this.yVelocity;
+	}
+	
+	public int getWidth() {
+		return this.width;
+	}
+	
+	public int getHeight() {
+		return this.height;
+	}
+	
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	
+	public Color getColor() {
+		return this.color;
+	}
 }
